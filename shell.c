57157b3a47169c266314405d4ca5c5049160cb20
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2023-11-15 17:51:14 +0100

    Handle exit errors

--- shell.c
+++ shell.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	ssize_t response;
-	char *exit_message = "(: Leaving so soon?\n(: See you next time...\n";
 
 	while (5)
 	{
@@ -18,7 +17,6 @@ int main(void)
 		/* If user types exit or EXIT break the loop*/
 		if (response == 99)
 		{
-			write(STDOUT_FILENO, exit_message, strlen(exit_message));
 			return (0);
 		}
 	}
@@ -50,7 +48,7 @@ ssize_t get_exec_command(void)
 	if (val == -1)
 	{
 		if (feof(stdin))
-			write(STDOUT_FILENO, "\n", 1), exit(EXIT_SUCCESS);
+			write(STDOUT_FILENO, "\n", 1);
 		else
 			perror("getline"), exit(EXIT_FAILURE);
 	}
@@ -61,16 +59,15 @@ ssize_t get_exec_command(void)
 	token = strtok(mesg_copy, delim);
 	while (token)
 		num_token++, token = strtok(NULL, delim);
-	num_token++; /* Cater for terminating NULL*/
-	argv = malloc(sizeof(char *) * num_token);
+	argv = malloc(sizeof(char *) * (num_token + 1)); /*Null terminate*/
 	if (argv == NULL)
-	{
 		perror("malloc"), exit(EXIT_FAILURE);
-	}
 	token = strtok(mesg_copy, delim); /*Write each token into argv*/
 	for (i = 0; token; i++)
 	{
 		argv[i] = strdup(token);
+		if (argv[i] == NULL)
+			break;
 		token = strtok(NULL, delim);
 	}
 	argv[i] = NULL, response = execute_command(argv); /* Execute argv*/
@@ -95,6 +92,10 @@ int execute_command(char **command)
 	/* If user type exit or EXIT return 99*/
 	if ((strcmp(command[0], "exit") == 0) || (strcmp(command[0], "EXIT") == 0))
 		return (99);
+
+	if (*command == NULL)
+		write(STDOUT_FILENO, "\n", 1);
+
 	val = fork();
 	if (val == -1)
 	{
